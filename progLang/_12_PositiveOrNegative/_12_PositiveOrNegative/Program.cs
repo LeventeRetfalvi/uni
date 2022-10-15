0377401d4107c702527ceb1a11f7bc1d260a58d9
Levente Rétfalvi <levente.retfalvi@github.com>
2022-10-15 14:22:40 +0200

    exercise 12 - positive or negative, practicing selection

---
+++ progLang/_12_PositiveOrNegative/_12_PositiveOrNegative/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _12_PositiveOrNegative
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number!");
+            Console.Write("n = ");
+            double n = double.Parse(Console.ReadLine());
+
+            // One 'if' is required in the selection.
+            if (n > 0)
+            {
+                Console.WriteLine("The {0} is positive.", n);
+            }
+            // Any number of 'else if' can be present.
+            else if (n < 0)
+            {
+                Console.WriteLine("The {0} is negative.", n);
+            }
+            // Only one or zero 'else' can be present in a selection.
+            else
+            {
+                Console.WriteLine("The {0} is not positive, neither negative.", n);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
