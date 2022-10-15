9bc588b83d1366b55f6deb0069d40923e360d84b
Levente Rétfalvi <levente.retfalvi@github.com>
2022-10-15 14:14:01 +0200

    Exercise 11 - Odd or Even

---
+++ progLang/_11_OddOrEven/_11_OddOrEven/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _11_OddOrEven
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number!");
+            Console.Write("n = ");
+            int n = int.Parse(Console.ReadLine());
+
+            if (n % 2 == 0)
+            {
+                Console.WriteLine("The {0} is even.", n);
+            }
+            else
+            {
+                Console.WriteLine("The {0} is odd.", n);
+            }
+            Console.ReadLine();
+        }
+    }
+}
