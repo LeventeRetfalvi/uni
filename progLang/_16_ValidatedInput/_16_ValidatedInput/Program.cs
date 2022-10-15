1ad20a3addd3324606ab2939305c2d838aa5c658
Levente Rétfalvi <levente.retfalvi@github.com>
2022-10-15 23:13:55 +0200

    exercise 16: validated input

---
+++ progLang/_16_ValidatedInput/_16_ValidatedInput/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _16_ValidatedInput
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n;
+            do
+            {
+                Console.Write("Enter an integer number from [10; 20): ");    
+            } while (!int.TryParse(Console.ReadLine(), out n) || n < 10 || 20 <= n);
+
+            double k;
+            do
+            {
+                Console.Write("Enter a real number from [10, 20): ");
+            } while (!double.TryParse(Console.ReadLine(), out k) || k < 10 || 20 <= k);
+        }
+    }
+}
