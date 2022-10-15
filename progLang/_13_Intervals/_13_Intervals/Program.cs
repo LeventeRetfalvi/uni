761de43c879643f505b2e9d2d1807f7952cc2f16
Levente Rétfalvi <levente.retfalvi@github.com>
2022-10-15 14:36:27 +0200

    exercise 13 - checking input & basic logical operators

---
+++ progLang/_13_Intervals/_13_Intervals/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _13_Intervals
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Please enter a number: ");
+            int n;
+            if (int.TryParse(Console.ReadLine(), out n))
+            {
+                Console.WriteLine("You entered {0}", n);
+            }
+            else
+            {
+                Console.WriteLine("Error! It is not a number!");
+                return;
+            }
+
+            // [-inf, 4) U [12; inf]
+            // 'AND' and 'OR' operators
+            // n <4 || 12 <= n
+            if (!(4 <= n && n < 12))
+            {
+                Console.WriteLine("The entered number is good!");
+            }
+            else
+            {
+                Console.WriteLine("The entered number is wrong!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
