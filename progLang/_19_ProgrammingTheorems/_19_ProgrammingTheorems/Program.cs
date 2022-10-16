753df85399e98f0d56ffcf715210808dcd10a9e4
Levente Rétfalvi <levente.retfalvi@github.com>
2022-10-16 20:15:30 +0200

    exercise 1.3

---
+++ progLang/_19_ProgrammingTheorems/_19_ProgrammingTheorems/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _19_ProgrammingTheorems
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Sum the numbers.
+            int sum = 0;
+            int multiply = 1;
+            int min = int.MaxValue;
+            bool hasEven = false;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("{0}. szám = ", i);
+                int n = int.Parse(Console.ReadLine());
+
+                sum += n;
+                multiply *= n;
+                if (n < min)
+                    min = n;
+                if (n % 2 == 0)
+                    hasEven = true;
+            }
+
+            Console.WriteLine("The sum of numbers: {0}", sum);
+            Console.WriteLine("The multiplication of numbers: {0}", sum);
+            Console.WriteLine("The smallest number: {0}", min);
+            Console.WriteLine("There is {0} even number.", hasEven ? "at least one" : "not any");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ progLang/exercises/_01_03_DistanceBetweenPoints/_01_03_DistanceBetweenPoints/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _01_03_DistanceBetweenPoints
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Find the distance between two given points.
+            // Ask for the coordinates
+            Console.Write("Enter the first point first coordinate(x1): ");
+            int x1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter the first point second coordinate(y1): ");
+            int y1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter the second point first coordinate(x2): ");
+            int x2 = int.Parse(Console.ReadLine());
+            Console.Write("Enter the second point second coordinate(y2): ");
+            int y2 = int.Parse(Console.ReadLine());
+
+            // Calculate the distance
+            double distnace = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            // Print the result.
+            Console.WriteLine("The distance between the points is {0:0.000}.", distnace);
+            Console.ReadLine();
+        }
+    }
+}
