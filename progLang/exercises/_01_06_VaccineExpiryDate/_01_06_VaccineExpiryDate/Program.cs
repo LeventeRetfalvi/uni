6302d24473b196d2525dbd6bdc5757bb41519fa0
Levente Rétfalvi <levente.retfalvi@github.com>
2022-10-16 21:41:27 +0200

    exercise 1.6

---
+++ progLang/exercises/_01_06_VaccineExpiryDate/_01_06_VaccineExpiryDate/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _01_06_VaccineExpiryDate
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Vaccination year: ");
+            int year = int.Parse(Console.ReadLine());
+            Console.Write("Vaccination month: ");
+            int month = int.Parse(Console.ReadLine());
+            Console.Write("Vaccination day: ");
+            int day = int.Parse(Console.ReadLine());
+
+            DateTime vaccineDate = new DateTime(year, month, day);
+            int expiryDays = 182;
+            Console.WriteLine(vaccineDate.AddDays(expiryDays).ToShortDateString());
+            
+        }
+    }
+}
