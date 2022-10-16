e5c11e38fa8413a0e62b989ce32b69a3650f5d2f
Levente Rétfalvi <levente.retfalvi@github.com>
2022-10-16 20:23:18 +0200

    exercises 1.4

---
+++ progLang/exercises/_1_4_DegreesToRadians/_1_4_DegreesToRadians/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _1_4_DegreesToRadians
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Convert degrees to radians
+            Console.Write("Enter degrees to convert: ");
+            double degrees = double.Parse(Console.ReadLine());
+            
+            double radians = degrees / Math.PI / 180;
+            double piRadians = degrees / 180;
+            
+            Console.WriteLine("Degree in radians: {0}", radians);
+            Console.WriteLine("Degree in pi-radians: {0}", piRadians);
+            Console.ReadLine();
+        }
+    }
+}
