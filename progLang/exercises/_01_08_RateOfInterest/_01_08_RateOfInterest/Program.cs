31f50e840af1fd889909aa9b92e4a99dea219020
Levente Rétfalvi <levente.retfalvi@github.com>
2022-10-16 22:16:01 +0200

    exercise 1.8

---
+++ progLang/exercises/_01_08_RateOfInterest/_01_08_RateOfInterest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _01_08_RateOfInterest
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Calculate the earned interests, by the formula: To(1 + p / 100) ^ n where
+            // To the principal value
+            // p the rate of interest
+            // n number of years
+            Console.Write("Enter the principal value (HUF): ");
+            int principal = int.Parse(Console.ReadLine());
+            Console.Write("Enter the number of years: ");
+            int years = int.Parse(Console.ReadLine());
+            Console.Write("Enter the rate of interest (%): ");
+            double rate = double.Parse(Console.ReadLine());
+
+            double interest = principal * Math.Pow(1 + rate / 100, years) / 1_000_000;
+            
+            Console.WriteLine($"Your interests will be (million HUF): {interest:0.000}");
+            Console.ReadLine();
+        }
+    }
+}
