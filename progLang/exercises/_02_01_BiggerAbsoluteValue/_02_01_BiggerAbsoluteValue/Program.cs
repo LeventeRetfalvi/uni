669f32fc6f10eef996f06664217022642260ce0d
Levente Rétfalvi <levente.retfalvi@github.com>
2022-10-17 06:55:47 +0200

    exercise 02_01

---
+++ progLang/exercises/_02_01_BiggerAbsoluteValue/_02_01_BiggerAbsoluteValue/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _02_01_BiggerAbsoluteValue
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter the first number: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Enter the second number: ");
+            int b = int.Parse(Console.ReadLine());
+
+            if (Math.Abs(a) > Math.Abs(b))
+            {
+                Console.WriteLine("The first number ({0}) has the bigger absolute value.", a);
+            }
+            else if (Math.Abs(a) < Math.Abs(b))
+            {
+                Console.WriteLine("The second number ({0}) has the bigger absolute value.", b);
+            }
+            else
+            {
+                Console.WriteLine("The two numbers ({a} and {b}) has the same absolute value.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
